a38677bdd1ea3ff34b9e55c7df6b8734a6886cf2
vrkarthik14 <vrkarthik14@gmail.com>
2020-05-02 13:53:43 +0530

    [ADD] Firebase auth added

--- app/src/main/java/com/ehrpatient/DashboardActivity.java
+++ app/src/main/java/com/ehrpatient/DashboardActivity.java
@@ -3,6 +3,9 @@ package com.ehrpatient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Toast.makeText(this, FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_SHORT).show();
     }
 }
